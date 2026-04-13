5340dfc3fa7ac3d7bb34a1dd387b62194b7254fc
maximkrouk <id.maximkrouk@gmail.com>
2026-04-13 10:01:34 +0200

    feat: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,77 +3,83 @@
 import PackageDescription
 
 let package = Package(
-  name: "swift-vapor-extensions",
-  platforms: [
-      .macOS(.v10_15),
-      .iOS(.v13),
-      .watchOS(.v6),
-      .tvOS(.v13),
-  ],
-  products: [
-    .library(
-      name: "FluentKitExtensions",
-      targets: ["FluentKitExtensions"]
-    ),
-    .library(
-      name: "GraphitiExtensions",
-      targets: ["GraphitiExtensions"]
-    ),
-    .library(
-      name: "GraphQLKitExtensions",
-      targets: ["GraphQLKitExtensions"]
-    ),
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/vapor/fluent-kit.git",
-      .upToNextMajor(from: "1.44.0")
-    ),
-    .package(
-      url: "https://github.com/graphqlswift/graphiti.git",
-      .upToNextMinor(from: "0.26.0")
-    ),
-    .package(
-      url: "https://github.com/alexsteinerde/graphql-kit.git",
-      .upToNextMajor(from: "2.4.0")
-    ),
-    .package(
-      url: "https://github.com/capturecontext/swift-prelude.git",
-      branch: "develop"
-    )
-  ],
-  targets: [
-    .target(
-      name: "FluentKitExtensions",
-      dependencies: [
-        .product(
-          name: "FluentKit",
-          package: "fluent-kit"
-        )
-      ]
-    ),
-    .target(
-      name: "GraphitiExtensions",
-      dependencies: [
-        .product(
-          name: "Graphiti",
-          package: "graphiti"
-        )
-      ]
-    ),
-    .target(
-      name: "GraphQLKitExtensions",
-      dependencies: [
-        .target(name: "GraphitiExtensions"),
-        .product(
-          name: "GraphQLKit",
-          package: "graphql-kit"
-        ),
-        .product(
-          name: "Prelude",
-          package: "swift-prelude"
-        )
-      ]
-    ),
-  ]
+	name: "swift-vapor-extensions",
+	platforms: [
+		.macOS(.v13),
+		.iOS(.v13),
+		.watchOS(.v6),
+		.tvOS(.v13),
+	],
+	products: [
+		.library(
+			name: "FluentKitExtensions",
+			targets: ["FluentKitExtensions"]
+		),
+		.library(
+			name: "GraphQLExtensions",
+			targets: ["GraphQLExtensions"]
+		),
+	],
+	dependencies: [
+		.package(
+			url: "https://github.com/vapor/fluent-kit.git",
+			.upToNextMajor(from: "1.44.0")
+		),
+		.package(
+			url: "https://github.com/graphqlswift/graphiti.git",
+			.upToNextMajor(from: "3.1.0")
+		),
+		.package(
+			url: "https://github.com/graphqlswift/graphql-vapor.git",
+			.upToNextMajor(from: "1.0.0")
+		),
+		.package(
+			url: "https://github.com/capturecontext/swift-keypaths-extensions.git",
+			.upToNextMajor(from: "0.2.0")
+		),
+		.package(
+			url: "https://github.com/capturecontext/swift-result-builders.git",
+			.upToNextMajor(from: "0.0.2")
+		),
+		.package(
+			url: "https://github.com/capturecontext/swift-function-composition.git",
+			branch: "tuples"
+		),
+	],
+	targets: [
+		.target(
+			name: "FluentKitExtensions",
+			dependencies: [
+				.product(
+					name: "FluentKit",
+					package: "fluent-kit"
+				)
+			]
+		),
+		.target(
+			name: "GraphQLExtensions",
+			dependencies: [
+				.product(
+					name: "KeyPathsExtensions",
+					package: "swift-keypaths-extensions"
+				),
+				.product(
+					name: "Graphiti",
+					package: "graphiti"
+				),
+				.product(
+					name: "GraphQLVapor",
+					package: "graphql-vapor"
+				),
+				.product(
+					name: "_Tuples",
+					package: "swift-function-composition"
+				),
+				.product(
+					name: "ArrayBuilder",
+					package: "swift-result-builders"
+				),
+			]
+		),
+	]
 )
